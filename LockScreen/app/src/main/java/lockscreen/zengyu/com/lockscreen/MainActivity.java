961ab49f6365e5a14befc5eba67944160c9d4944
曾宇 <zengyu@meizu.com>
2018-11-19 11:02:38 +0800

    修复锁屏后出现在任务列表中的bug，增加自动跳转授权页失败时的提示，增加无法卸载的提示

--- LockScreen/app/src/main/java/lockscreen/zengyu/com/lockscreen/MainActivity.java
+++ LockScreen/app/src/main/java/lockscreen/zengyu/com/lockscreen/MainActivity.java
@@ -1,11 +1,14 @@
 package lockscreen.zengyu.com.lockscreen;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Process;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -23,8 +26,11 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
-        android.os.Process.killProcess(android.os.Process.myPid());
+        try {
+            super.onDestroy();
+        } finally {
+            Process.killProcess(Process.myPid());
+        }
     }
 
     private void init() {
@@ -37,6 +43,7 @@ public class MainActivity extends Activity {
             if (!manager.isAdminActive(componentName)) {
                 Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
                 intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName);
+                intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, getString(R.string.policy_content));
                 startActivityForResult(intent, PERMISSION_REQUEST_CODE);
             } else {
                 lock();
@@ -48,8 +55,21 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PERMISSION_REQUEST_CODE && resultCode == RESULT_OK) {
-            lock();
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                lock();
+            } else if (resultCode == RESULT_CANCELED) {
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.app_name)
+                        .setMessage(R.string.dialog_content).setCancelable(false)
+                        .setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+                        .create().show();
+            }
         } else {
             finish();
         }
